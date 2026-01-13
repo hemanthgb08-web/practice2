e46b3cd6228f99d32fbcc78af135329049be8672
Jayanth Jain <jayanth.jain@crecientech.com>
2026-01-13 21:47:43 +0530

    firssdt commit to github

---
+++ java/test21.java
@@ -0,0 +1,25 @@
+public class SampleProgram {
+
+    // Main method - program execution starts here
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Java Programming!");
+
+        int a = 10;
+        int b = 20;
+
+        int sum = add(a, b);
+        System.out.println("Sum of " + a + " and " + b + " is: " + sum);
+
+        if (sum > 20) {
+            System.out.println("Sum is greater than 20");
+        } else {
+            System.out.println("Sum is less than or equal to 20");
+        }
+    }
+
+    // Method to add two numbers
+    public static int add(int x, int y) {
+        return x + y;
+    }
+}
