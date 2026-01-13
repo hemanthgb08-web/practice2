0cdfffc3ac9c9a27425fb866b776c2f2f4121467
Jayanth Jain <jayanth.jain@crecientech.com>
2026-01-13 22:42:32 +0530

    adding 2 new files

---
+++ java/test23.java
@@ -0,0 +1,14 @@
+public class SampleProgram {
+
+    // Main method - program execution starts here
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Java Programming!");
+
+        int a = 10;
+        int b = 20;
+
+        int sum = add(a, b);
+        System.out.println("Sum of " + a + " and " + b + " is: " + sum);
+
+}
